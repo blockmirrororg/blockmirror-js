b3a0589dfbb17819eb5e3a62c87630f99f3c88f5
ghter123 <ghter123@hotmail.com>
2019-05-27 17:38:34 +0800

    小修改

--- tests/chain.test.js
+++ tests/chain.test.js
@@ -1,4 +1,54 @@
 const axios = require("../src/axios");
+const Transaction = require("../src/transaction");
+
+const Generator = new Transaction();
+const bp1Priv = "95D4B0DF5B1069B47F35C8C7A6764BB8B760D0359B6C1221DDCB46CE5830E14C";
+
+beforeEach(async () => {
+  await axios.post(
+    "chain/transaction",
+    JSON.stringify(
+      Generator.createNewFormat(
+        [bp1Priv],
+        {
+          name: "A股",
+          desc: "代表五个浮点 股价上限 下限 最高 最低 平均 ",
+          dataFormat: "0101010101",
+          validScript: "01",
+          resultScript: "02",
+        },
+        1000000,
+        0,
+      ),
+    ),
+  );
+
+  await axios.post(
+    "chain/transaction",
+    JSON.stringify(
+      Generator.createNewDataType(
+        [bp1Priv],
+        {
+          format: "A股",
+          name: "sz000001",
+          desc: "国内某股票",
+        },
+        1000000,
+        0,
+      ),
+    ),
+  );
+});
+
+test("test for chain/transaction post NewFormat 'A股' and get", async () => {
+  const format = await axios.get(`chain/format?${encodeURIComponent("A股")}`);
+  expect(format).toHaveProperty("name", "A股");
+});
+
+test("test for chain/transaction post NewDataType 'sz000001' and get", async () => {
+  const dataType = await axios.get("chain/datatypes?sz000001");
+  expect(dataType).toHaveProperty("name", "sz000001");
+});
 
 test("test for get chain/status ", async () => {
   const status = await axios.get("chain/status");
@@ -17,9 +67,12 @@ test("test for post chain/data", async () => {
     buf.writeFloatBE(args[i], i * 4);
   }
   expect(
-    await axios.post("chain/data", {
-      name: "sz000001",
-      data: buf.toString("hex"),
-    }),
+    await axios.post(
+      "chain/data",
+      JSON.stringify({
+        name: "sz000001",
+        data: buf.toString("hex"),
+      }),
+    ),
   ).toHaveReturned();
 });
