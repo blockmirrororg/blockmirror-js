9c6039a954312450292a7c54f9b21e1ed2d6add5
ghter123 <ghter123@hotmail.com>
2019-05-28 13:59:22 +0800

    update

--- tests/chain.test.js
+++ tests/chain.test.js
@@ -1,51 +1,11 @@
 const axios = require("../src/axios");
-const Transaction = require("../src/transaction");
 
-const Generator = new Transaction();
-const bp1Priv = "95D4B0DF5B1069B47F35C8C7A6764BB8B760D0359B6C1221DDCB46CE5830E14C";
-
-beforeEach(async () => {
-  await axios.post(
-    "chain/transaction",
-    JSON.stringify(
-      Generator.createNewFormat(
-        [bp1Priv],
-        {
-          name: "A股",
-          desc: "代表五个浮点 股价上限 下限 最高 最低 平均 ",
-          dataFormat: "0101010101",
-          validScript: "01",
-          resultScript: "02",
-        },
-        1000000,
-        0,
-      ),
-    ),
-  );
-
-  await axios.post(
-    "chain/transaction",
-    JSON.stringify(
-      Generator.createNewDataType(
-        [bp1Priv],
-        {
-          format: "A股",
-          name: "sz000001",
-          desc: "国内某股票",
-        },
-        1000000,
-        0,
-      ),
-    ),
-  );
-});
-
-test("test for chain/transaction post NewFormat 'A股' and get", async () => {
+test("test get format 'A股'", async () => {
   const format = await axios.get(`chain/format?${encodeURIComponent("A股")}`);
   expect(format).toHaveProperty("name", "A股");
 });
 
-test("test for chain/transaction post NewDataType 'sz000001' and get", async () => {
+test("test get data type 'sz000001'", async () => {
   const dataType = await axios.get("chain/datatypes?sz000001");
   expect(dataType).toHaveProperty("name", "sz000001");
 });
@@ -57,15 +17,19 @@ test("test for get chain/status ", async () => {
 
 test("test for get chain/bps ", async () => {
   const bps = await axios.get("chain/bps");
-  expect(bps).toHaveReturned();
+  expect(bps.length).toBeGreaterThanOrEqual(1);
 });
 
 test("test for post chain/data", async () => {
   const args = [110.22, 115.1, 160.2, 100.3, 130.22];
   const buf = Buffer.alloc(args.length * 4);
   for (let i = 0; i < args.length; i++) {
-    buf.writeFloatBE(args[i], i * 4);
+    buf.writeFloatLE(args[i], i * 4)
   }
+  console.log(JSON.stringify({
+    name: "sz000001",
+    data: buf.toString("hex"),
+  }))
   expect(
     await axios.post(
       "chain/data",
--- tests/teardown.js
+++ tests/teardown.js
@@ -1,4 +1,6 @@
+const axios = require("../src/axios");
 
 module.exports = async function () {
   console.log("Exiting blockmirror tests");
+  await axios.get('node/stop');
 }
