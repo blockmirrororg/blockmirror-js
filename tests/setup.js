52d7d7056475fecfaece9717f0e9e185e5dcadfc
ghter123 <ghter123@hotmail.com>
2019-05-28 14:41:46 +0800

    增加异常处理

--- tests/setup.js
+++ tests/setup.js
@@ -3,52 +3,101 @@ const axios = require("../src/axios");
 const Transaction = require("../src/transaction");
 
 const Generator = new Transaction();
-const bp1Priv = "95D4B0DF5B1069B47F35C8C7A6764BB8B760D0359B6C1221DDCB46CE5830E14C";
+const bp1Priv =
+  "95D4B0DF5B1069B47F35C8C7A6764BB8B760D0359B6C1221DDCB46CE5830E14C";
 
 module.exports = async function() {
   console.log("Initializing blockmirror tests");
   spawn.sync("rm", ["-rf", "../running"], { stdio: "inherit" });
   spawn.sync("mkdir", ["../running"], { stdio: "inherit" });
-  await spawn("../build/test/test_network", ["../config.json"], {
+  const rpc = await spawn("../build/test/test_network", ["../config.json"], {
     stdio: "inherit",
     cwd: "../running",
+    detached: true,
   });
-  await beforeAll();
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  try {
+    await beforeAll();
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  } catch (error) {
+    console.log(error.message);
+    process.kill(-rpc.pid);
+    throw new Error();
+  }
 };
 
 // eslint-disable-next-line require-jsdoc
 async function beforeAll() {
-  await axios.post(
-    "chain/transaction",
-    JSON.stringify(
-      Generator.createNewFormat(
-        [bp1Priv],
-        {
-          name: "A股",
-          desc: "代表五个浮点 股价上限 下限 最高 最低 平均 ",
-          dataFormat: "0101010101",
-          validScript: "01",
-          resultScript: "02",
-        },
-        1000000,
-        0,
-      ),
-    ),
-  );
-  await axios.post(
-    "chain/transaction",
-    JSON.stringify(
-      Generator.createNewDataType(
-        [bp1Priv],
-        {
-          format: "A股",
-          name: "sz000001",
-          desc: "国内某股票",
-        },
-        1000000,
-        0,
-      ),
-    ),
-  );
+  try {
+    const format = Generator.createNewFormat(
+      [bp1Priv],
+      {
+        name: "A股",
+        desc: "代表五个浮点 股价上限 下限 最高 最低 平均 ",
+        dataFormat: "0101010101",
+        validScript: "01",
+        resultScript: "02",
+      },
+      1000000,
+      0,
+    );
+
+    console.log({
+      at: "beforeAll",
+      action: "post NewFormat",
+      status: "starting",
+      context: JSON.stringify(format),
+    });
+
+    await axios.post("chain/transaction", JSON.stringify(format));
+
+    console.log({
+      at: "beforeAll",
+      action: "post Format",
+      status: "successed",
+    });
+  } catch (error) {
+    console.log({
+      at: "beforeAll",
+      action: "post NewFormat",
+      status: "error",
+      message: error.message,
+    });
+    throw new Error("beforAll失败！");
+  }
+
+  try {
+    const dataType = Generator.createNewDataType(
+      [bp1Priv],
+      {
+        format: "A股",
+        name: "sz000001",
+        desc: "国内某股票",
+      },
+      1000000,
+      0,
+    );
+    console.log({
+      at: "beforeAll",
+      action: "post DataType",
+      status: "starting",
+      context: JSON.stringify(dataType),
+    });
+
+    await axios.post("chain/transaction", JSON.stringify(dataType));
+
+    console.log({
+      at: "beforeAll",
+      action: "post DataType",
+      status: "successed",
+    });
+  } catch (error) {
+    console.log({
+      at: "beforeAll",
+      action: "post DataType",
+      status: "error",
+      message: error.message,
+    });
+    throw new Error("beforAll失败！");
+  }
 }
