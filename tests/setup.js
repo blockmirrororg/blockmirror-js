f623e990d07b2e5bb72486b87b9d4f22b36b0f0c
ghter123 <ghter123@hotmail.com>
2019-05-29 14:53:17 +0800

    update

--- tests/setup.js
+++ tests/setup.js
@@ -3,7 +3,8 @@ const axios = require("../src/axios");
 const Transaction = require("../src/transaction");
 
 const Generator = new Transaction();
-const bp1Priv = "95D4B0DF5B1069B47F35C8C7A6764BB8B760D0359B6C1221DDCB46CE5830E14C";
+const bp1Priv =
+  "95D4B0DF5B1069B47F35C8C7A6764BB8B760D0359B6C1221DDCB46CE5830E14C";
 
 module.exports = async function() {
   console.log("Initializing blockmirror tests");
@@ -20,6 +21,7 @@ module.exports = async function() {
     detached: true,
   });
 
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   try {
     await beforeAll();
     await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -32,7 +34,7 @@ module.exports = async function() {
 
 // eslint-disable-next-line require-jsdoc
 async function beforeAll() {
-  let format
+  let format;
   try {
     format = Generator.createNewFormat(
       [bp1Priv],
@@ -66,7 +68,7 @@ async function beforeAll() {
     throw new Error("beforAll失败！");
   }
 
-  let dataType
+  let dataType;
   try {
     dataType = Generator.createNewDataType(
       [bp1Priv],
