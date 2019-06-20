a4099ab5712baf9252ca159b7804b224c273feb0
ghter123 <ghter123@hotmail.com>
2019-06-20 10:14:51 +0800

    切换一个更稳定的交易所

--- src/collector/gateio.js
+++ src/collector/gateio.js
@@ -3,28 +3,30 @@ const CoinCodes = require("../../CoinList");
 const CCXT = require("ccxt");
 
 const coinEmitter = new Emitter();
+const ex = new CCXT.binance();
 const gateio = new CCXT.gateio();
 
-const fetch = async() => {
-  const data = await gateio.fetchTickers();
+const fetch = async () => {
+  const data = await ex.fetchTickers();
+  const rmbPrice = await gateio.fetchTicker("USDT/CNYX");
   const result = CoinCodes.map((o) => {
     return {
       code: o,
-      data: [data[`${o}/USDT`].ask, data[`${o}/CNYX`].ask],
+      data: [data[`${o}/USDT`].ask, rmbPrice.ask * data[`${o}/USDT`].ask],
     };
   });
+  console.log(result)
   coinEmitter.emit("insert", result);
   setTimeout(fetch, 1 * 1000);
 };
 
-(async()=>await fetch())()
-
+(async () => await fetch())();
 
 module.exports = {
   addListener(event, listener) {
     coinEmitter.addListener(event, listener);
     return () => {
-        coinEmitter.removeListener(event, listener);
+      coinEmitter.removeListener(event, listener);
     };
   },
 };
